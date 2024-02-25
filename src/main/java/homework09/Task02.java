7982bab24ffd71d6cbdc98cc6f869ff8000cd90c
JelenaRjazanska1972 <160174934+JelenaRjazanska1972@users.noreply.github.com>
2024-02-25 21:06:53 +0100

    initial commit Homework09

--- src/main/java/homework09/Task02.java
+++ src/main/java/homework09/Task02.java
@@ -4,23 +4,45 @@ import java.util.Scanner;
 
 public class Task02 {
     public static void main(String[] args) {
-        int number = (int) (Math.random() * 101);
-        System.out.println(number);
-        int counter = 0;
-       int numberNext=0;
+        Scanner scanner = new Scanner(System.in);
+        char gameOver;
         do {
-            System.out.println("Угадайте загаданное число!");
-            Scanner scanner = new Scanner(System.in);
-            numberNext = scanner.nextInt();
-            if (numberNext < number) {
-                System.out.println("Ваше число меньше загаданного! Пробуйте еще раз!");
-                counter = counter++;
-            } else {
-                System.out.println("Ваше число больше загаданного! Пробуйте еще раз!");
-                counter = counter++;
-            }} while (counter != 5 || numberNext==number);
-        System.out.println("Бинго!");
-        System.out.println("Число попыток!" + counter);
-    }
-}
+            int number = (int) (Math.random() * 101);
+            System.out.println(number);
+            int counter = 0;
+            int maxCounter = 3;
+            System.out.println("Угадайте загаданное число от 1 - 100! У Вас " + maxCounter + "попыток");
+            while (true) {
+                System.out.println("Введите число ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Ошибка введите число!");
+                    scanner.next();
+                }
+                int numberNext = scanner.nextInt();
+                if (numberNext < 1 || numberNext > 100) {
+                    System.out.println("Число должно быть в диапазоне 1 - 100!");
+                    continue;
+                }
+                counter++;
+                if ((numberNext < number)) {
+                    System.out.println("Ваше число меньше загаданного! Пробуйте еще раз!");
+                } else if (numberNext > number) {
+                    System.out.println("Ваше число больше загаданного! Пробуйте еще раз!");
+                } else {
+                    System.out.println("Бинго!" + number);
+                    break;
+                }
+                if (counter >= maxCounter) {
+                    System.out.println("Вы использовали все попытки!");
+                    break;
+                }
+            }
+                        System.out.println("Для продолжения выберете y и для выхода n");
+                        gameOver=scanner.next().toLowerCase().charAt(0);
+            }
+            while (gameOver=='y');
+            System.out.println("Спасибо за игру!");
+            scanner.close();
+
+    }}
 
